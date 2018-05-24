34175c1fc4bfbd500099696bc02578f10e1fbfde
Jason Young <thinkbigthings@gmail.com>
2018-05-24 10:51:53 -0400

    added header and styling like create-react-app template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Game from './Game';
+import App from './App';
 
 import './index.css';
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
